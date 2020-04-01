c1dbb9f2503eff37e510556a0a234f8e98d07084
Hunter Gilliam <huntergilliam03@gmail.com>
2020-04-01 11:06:51 -0500

    Initial Commit

---
+++ MusicTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicTimer
+//
+//  Created by Hunter Gilliam on 4/1/20.
+//  Copyright © 2020 Hunter Gilliam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
