051c22e0c24a4b7a5d5257e3e6168603051afa68
Hunter Gilliam <huntergilliam03@gmail.com>
2020-04-01 12:16:46 -0500

    - Initial commit - Made UI Programmatic - Started on first VC

--- MusicTimer/ViewController.swift
+++ MusicTimer/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: UI Variables
+    private let playButton : UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 100, height: 50))
+        button.setTitle("Play", for: .normal)
+        button.backgroundColor = .blue
+        return button
+    }()
+    
+    // MARK: VIEW DIDLOAD
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    func setupView() {
+        
+    }
+    
+    func buildButtons() {
+        view.addSubview(playButton)
+        
     }
 
 
