3764fea742156d453df9445104f116a010e15a20
Hunter Gilliam <huntergilliam03@gmail.com>
2020-04-13 15:55:59 -0500

    Removed testAPI function and privated some variables

--- MusicTimer/Models/MusicPlayer.swift
+++ MusicTimer/Models/MusicPlayer.swift
@@ -10,8 +10,8 @@ import MediaPlayer
 import StoreKit
 
 class MusicPlayer {
-    let devToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhEOFY3SExVR1EifQ.eyJpc3MiOiJFN1k0NTRRVE41IiwiaWF0IjoxNTg2Mjk1Nzc4LCJleHAiOjE2MDE4NDc3Nzh9.lyTuDTQ0FOdQq_Is2YC1RydTtRsfEPN2-2kY4GQLxbFpxGBiym-3q_N5GPd5d2va4uipMIQeJv_tK8DAy7Ellw" // Invalid After 10/4/2020
-    let controller = SKCloudServiceController()
+    private let devToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhEOFY3SExVR1EifQ.eyJpc3MiOiJFN1k0NTRRVE41IiwiaWF0IjoxNTg2Mjk1Nzc4LCJleHAiOjE2MDE4NDc3Nzh9.lyTuDTQ0FOdQq_Is2YC1RydTtRsfEPN2-2kY4GQLxbFpxGBiym-3q_N5GPd5d2va4uipMIQeJv_tK8DAy7Ellw" // Invalid After 10/4/2020
+    private let controller = SKCloudServiceController()
     
     private var userToken = ""
     private var userStorefront = ""
@@ -64,36 +64,6 @@ class MusicPlayer {
                 print(object)
             } catch {
                 print(error)
-            }
-        }.resume()
-    }
-    
-    func testAPIRequest() {
-        
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "api.music.apple.com"
-        components.path = "/v1/catalog/\(userStorefront)/search"
-        
-        components.queryItems = [
-            URLQueryItem(name: "term", value: "workouts"),
-            URLQueryItem(name: "limit", value: "25"),
-            URLQueryItem(name: "types", value: "playlists")
-        ]
-        
-        let url = components.url
-        
-        var request = URLRequest(url: url!)
-        request.setValue("Bearer \(devToken)", forHTTPHeaderField: "Authorization")
-        request.setValue(userToken, forHTTPHeaderField: "Music-User-Token")
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else { fatalError() }
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: [])
-                print(json)
-            } catch {
-                
             }
         }.resume()
     }
